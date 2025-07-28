288d370d48ab9ffc9d3547d2d7d076769600a31a
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-07-28 22:48:10 +0500

    for getting  jwt token

--- departments/urls.py
+++ departments/urls.py
@@ -1,12 +1,15 @@
+# yourapp/urls.py
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DepartmentsViewSet
-
-
+from .views import DepartmentsViewSet, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r"departments", DepartmentsViewSet, basename="department")
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- departments/views.py
+++ departments/views.py
@@ -2,6 +2,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
 from .models import Departments
 from .serializers import DepartmentSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
+
 
 class DepartmentsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Departments.objects.all()
@@ -9,3 +12,25 @@ class DepartmentsViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['name']
     search_fields = ['name', 'description']
+
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        # agar status_code 200 bo‘lsa, user haqida ma’lumot qo‘shamiz
+        if response.status_code == 200:
+            user = self.get_user(request.data.get('username'))
+            response.data['user'] = {
+                'username': user.username,
+                'is_superuser': user.is_superuser
+            }
+        return response
+
+    def get_user(self, username):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        return User.objects.get(username=username)
