f8398dd8df840980b5e90ebb0b8d1c14840093ff
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-03 16:23:27 +0500

    admin.py optimized

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('first_name', 'last_name', 'phone', 'role', 'is_staff')
+    search_fields = ('phone', 'first_name', 'last_name', 'email')
+
+
+    fieldsets = (
+        (None, {'fields': ('phone', 'password')}),
+        ('Shaxsiy ma`lumotlar', {'fields': ('first_name', 'last_name', 'email', 'birth_date', 'gender')}),
+        ('Ruxsatlar', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
+        ('Tizim haqida', {'fields': ('last_login', 'date_joined')}),
+    )
+
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone', 'password1', 'password2', 'role', 'is_active')
+        }),
+    )
+    ordering = ('-date_joined',)
+
+admin.site.register(User, UserAdmin)
+
