10110cb1c46ce988fa807e44dabb4403024920be
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-06 11:47:19 +0500

    urls.py updated

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('departments.urls')),
+    path('api/', include('users.urls')),
+
 
     # Swagger UI
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
