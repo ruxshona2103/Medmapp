83184956d55bb8a837717e7e80e1970b2d1a4ddd
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-10 18:46:57 +0500

    updated (10.09.2025)

--- departments/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Departments
-
-admin.site.register(Departments)
--- departments/translation.py
+++
@@ -1,6 +0,0 @@
-from modeltranslation.translator import register, TranslationOptions
-from .models import Departments
-
-@register(Departments)
-class DepartmentsTranslationOptions(TranslationOptions):
-    fields = ("name", "description")
