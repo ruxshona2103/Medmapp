d263ed1751dfc5a634b5590f9cd3ab81d7d698b8
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-16 10:56:40 +0500

    users.py updated (16.09.2025)

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ClinicsConfig(AppConfig):
+class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'clinics'
+    name = 'authentication'
