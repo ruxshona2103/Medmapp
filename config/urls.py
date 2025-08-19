df9a9f3a672a32dcdb068a4baa78e1f1e500e394
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-19 10:13:48 +0500

    config.py updated

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
     path('api/', include('patients.urls')),
     path('api/', include('authentication.urls')),
 
