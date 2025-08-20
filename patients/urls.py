c12396fec8a84a8b2a71d4c22da3d1ccf93ca447
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-20 18:33:33 +0500

    patients.py updated(20.08.2025)

--- patients/urls.py
+++ patients/urls.py
@@ -1,12 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ApplicationViewSet, ServiceViewSet, OrderedServiceViewSet
-
-router = DefaultRouter()
-router.register(r'applications', ApplicationViewSet, basename="applications")
-router.register(r'services', ServiceViewSet, basename="services")
-router.register(r'ordered-services', OrderedServiceViewSet, basename="ordered-services")
+from django.urls import path
+from .views import PatientProfileView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("profile/me/", PatientProfileView.as_view(), name="patient-profile"),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,59 +1,10 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from .models import (
-    PatientProfile, Application, Document, Service,
-    OrderedService, ServiceStatusHistory
-)
-from .serializers import (
-    PatientProfileSerializer, ApplicationSerializer, DocumentSerializer,
-    ServiceSerializer, OrderedServiceSerializer, ServiceStatusHistorySerializer
-)
+from rest_framework import generics, permissions
+from .serializers import PatientProfileSerializer
 
-class PatientProfileViewSet(viewsets.ModelViewSet):
-    queryset = PatientProfile.objects.all()
+
+class PatientProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = PatientProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-class DocumentViewSet(viewsets.ModelViewSet):
-    queryset = Document.objects.all()
-    serializer_class = DocumentSerializer
-
-class ApplicationViewSet(viewsets.ModelViewSet):
-    queryset = Application.objects.all()
-    serializer_class = ApplicationSerializer
-
-    @action(detail=True, methods=["post"], url_path="add-document")
-    def add_document(self, request, pk=None):
-        application = self.get_object()
-        serializer = DocumentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(application=application)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ServiceViewSet(viewsets.ModelViewSet):
-    queryset = Service.objects.all()
-    serializer_class = ServiceSerializer
-
-
-class OrderedServiceViewSet(viewsets.ModelViewSet):
-    queryset = OrderedService.objects.all()
-    serializer_class = OrderedServiceSerializer
-
-    @action(detail=True, methods=["post"], url_path="update-status")
-    def update_status(self, request, pk=None):
-        """Buyurtma xizmatining statusini o‘zgartirish"""
-        ordered_service = self.get_object()
-        serializer = ServiceStatusHistorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(ordered_service=ordered_service)
-            ordered_service.current_status_index = ordered_service.status_history.count() - 1
-            ordered_service.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ServiceStatusHistoryViewSet(viewsets.ModelViewSet):
-    queryset = ServiceStatusHistory.objects.all()
-    serializer_class = ServiceStatusHistorySerializer
+    def get_object(self):
+        return self.request.user.patient_profile
