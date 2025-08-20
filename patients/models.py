dd2848d416a8c0d6f3b4f5989a2a0352473eaecd
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-20 18:33:43 +0500

    patients.py updated(20.08.2025)

--- patients/models.py
+++ patients/models.py
@@ -1,71 +1,35 @@
-from django.conf import settings
 from django.db import models
-
+from django.conf import settings
+from django.core.validators import RegexValidator, EmailValidator
 
 class PatientProfile(models.Model):
-    GENDER_CHOICES = (
-        ("Erkak", "Erkak"),
-        ("Ayol", "Ayol"),
-    )
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='patient_profile')
-    fullName = models.CharField(max_length=255)
-    passport = models.CharField(max_length=20)
-    dob = models.DateField()
-    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
-    phone = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
+    GENDER_CHOICES = [
+        ("male", "Erkak"),
+        ("female", "Ayol"),
+    ]
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="patient_profile", verbose_name="Bemor")
+    full_name = models.CharField(max_length=200, verbose_name="Ism-sharif")
+    passport = models.CharField(max_length=20, unique=True, verbose_name="Passport raqami")
+    dob = models.DateTimeField(verbose_name="Tug'ilgan sana")
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, verbose_name="Jinsi")
+    phone = models.CharField(max_length=20,
+                             validators=[RegexValidator(regex=r"^\+998\d{9}$",
+                                                        message="Telefon raqam +998 bilan boshlanishi va 13 ta belgidan iborat bo‘lishi kerak.")],
+                             verbose_name="Telefon raqami"
+                             )
+    email = models.EmailField(max_length=100,
+                              validators=[EmailValidator(message="Yaroqli email kiriting(@-belgisi bo'lishi kerak )")],
+                              verbose_name="Elektron pochta"
+                              )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Yaratilgan vaqt")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Oxirgi yangilanish")
+
+    class Meta:
+        verbose_name = "Bemor Profili"
+        verbose_name_plural = "Bemor Profillari"
+        ordering = ["-created_at"]
 
     def __str__(self):
-        return f"{self.fullName} ({self.user.username})"
+        return f"{self.full_name}"
 
-class Application(models.Model):
-    STATUS_CHOICES = (
-        ("new", "new"),
-        ("processing", "processing"),
-        ("completed", "completed"),
-    )
-    patient = models.ForeignKey(PatientProfile, on_delete=models.CASCADE, related_name='applications')
-    complaint = models.TextField()
-    diagnosis = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=50, default='new', choices=STATUS_CHOICES)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Application #{self.id} - {self.patient.username} - {self.status}"
-
-class Document(models.Model):
-    application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name='documents')
-    file = models.FileField(upload_to='documents/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Document {self.id} for App {self.application.id}"
-
-class Service(models.Model):
-    service_id = models.CharField(max_length=50, primary_key=True)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    price_description = models.CharField(max_length=255)
-    icon_class = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.name} ({self.service_id})"
-
-class OrderedService(models.Model):
-    application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name='ordered_services')
-    service = models.ForeignKey(Service, on_delete=models.PROTECT, related_name='orders')
-    order_date = models.DateTimeField(auto_now_add=True)
-    data = models.JSONField(null=True, blank=True)
-    current_status_index = models.IntegerField(default=0)
-
-    def __str__(self):
-        return f"OrderedService {self.id} - {self.service.name} for App {self.application.id}"
-
-class ServiceStatusHistory(models.Model):
-    ordered_service = models.ForeignKey(OrderedService, on_delete=models.CASCADE, related_name='status_history')
-    status_text = models.CharField(max_length=255)
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Status {self.status_text} at {self.date}"
