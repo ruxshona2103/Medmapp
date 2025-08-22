0d9a10363b892dc5fe20a8e4f3f4afb4eb087403
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-22 12:41:55 +0500

    appliactions app  updated (22.08.2025)

--- applications/urls.py
+++ applications/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import ApplicationCreateView, ApplicationStatusView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    ApplicationCreateView,
+    ApplicationStatusView,
+    ApplicationViewSet
+)
+
+router = DefaultRouter()
+router.register(r'applications', ApplicationViewSet, basename="applications")
 
 urlpatterns = [
     path("application/create/", ApplicationCreateView.as_view(), name="application-create"),
     path("application/status/", ApplicationStatusView.as_view(), name="application-status"),
+    path("", include(router.urls)),  # -> /applications/ ishlaydi
 ]
