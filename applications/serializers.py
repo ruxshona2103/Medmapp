fd16fbbbb458e7adfd321b7d2e899c01144adda3
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-22 12:42:01 +0500

    appliactions app  updated (22.08.2025)

--- applications/serializers.py
+++ applications/serializers.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
 from .models import Application
 
+from rest_framework import serializers
+from .models import Application
+
+
 class ApplicationSerializer(serializers.ModelSerializer):
+    status_display = serializers.CharField(source="get_status_display", read_only=True)
+
     class Meta:
         model = Application
-        fields = "__all__"
-        read_only_fields = ["patient", "status", "created_at"]
+        fields = [
+            "id",
+            "application_id",
+            "clinic_name",
+            "complaint",
+            "diagnosis",
+            "status",
+            "status_display",
+            "created_at",
+            "updated_at"
+        ]
+        read_only_fields = ["application_id", "status", "created_at", "updated_at"]
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data["patient"] = request.user
+        return super().create(validated_data)
+
 
