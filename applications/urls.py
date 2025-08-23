79003da1b42817e45be8b27413f06e2137c3c4c5
Ruxshona Aglamkhujayeva <ruxshonaaglamkhujayeva@gmail.com>
2025-08-23 08:20:00 +0500

    documents created (23.08.2025)

--- applications/urls.py
+++ applications/urls.py
@@ -3,7 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     ApplicationCreateView,
     ApplicationStatusView,
-    ApplicationViewSet
+    ApplicationViewSet,
+    DocumentCreateView,
+    DocumentListView
 )
 
 router = DefaultRouter()
@@ -12,5 +14,7 @@ router.register(r'applications', ApplicationViewSet, basename="applications")
 urlpatterns = [
     path("application/create/", ApplicationCreateView.as_view(), name="application-create"),
     path("application/status/", ApplicationStatusView.as_view(), name="application-status"),
+    path("documents/create/", DocumentCreateView.as_view(), name="document-create"),
+    path("documents/", DocumentListView.as_view(), name="document-list"),
     path("", include(router.urls)),  # -> /applications/ ishlaydi
 ]
