90f60e9d754dd3061d66ad40aa41ea24a619fc90
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-08-27 00:12:49 +0500

    fix

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2_yzlz!b-z%j+p4e^^^!ewhmg%5r==5u)24t*s+j^xun80s14_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['medmapp.onrender.com', '127.0.0.1', 'localhost']
 
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
