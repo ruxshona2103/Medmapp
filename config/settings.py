caa19438cce9710af255bc49b53764a2828d2747
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-09-19 22:35:38 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 import os
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
 from django.utils.translation import gettext_lazy as _
 BASE_DIR = Path(__file__).resolve().parent.parent
 
