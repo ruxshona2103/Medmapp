bdfd3a56d092eed183d1fafb268d42945c5dad87
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-09-19 22:53:24 +0500

    fixed

--- patients/urls.py
+++ patients/urls.py
@@ -16,18 +16,18 @@ from .views import (
 urlpatterns = [
     # Profil va bemorlar
     path("profiles/", PatientProfileListView.as_view(), name="patient-profile-list"),
-    path("patients/", PatientListView.as_view(), name="patient-list"),
-    path("patients/create/", PatientCreateView.as_view(), name="patient-create"),
-    path("patients/me/", PatientMeView.as_view(), name="patient-me"),
+    path("", PatientListView.as_view(), name="patient-list"),
+    path("create/", PatientCreateView.as_view(), name="patient-create"),
+    path("me/", PatientMeView.as_view(), name="patient-me"),
 
     # Operator
     path("operators/me/", OperatorMeView.as_view(), name="operator-me"),
 
     # Bosqich
-    path("patients/<int:patient_id>/change-stage/", ChangeStageView.as_view(), name="change-stage"),
+    path("<int:patient_id>/change-stage/", ChangeStageView.as_view(), name="change-stage"),
 
     # Hujjatlar
-    path("patients/<int:patient_id>/documents/", PatientDocumentCreateView.as_view(), name="patient-document-create"),
+    path("<int:patient_id>/documents/", PatientDocumentCreateView.as_view(), name="patient-document-create"),
     path("documents/<int:pk>/", PatientDocumentDeleteView.as_view(), name="patient-document-delete"),
 
     # Stage va Tag
