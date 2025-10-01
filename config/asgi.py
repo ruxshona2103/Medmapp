149e010a3961bd46b29a8d644dd8df59b216f36b
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-10-01 11:14:07 +0500

    fixed

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,13 @@
 import os
 from django.core.asgi import get_asgi_application
 
-# ⚠️ Birinchi bo‘lib DJANGO_SETTINGS_MODULE o‘rnatiladi
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-# Endi Django application initialize qilinadi
 django_asgi_app = get_asgi_application()
 
-# Keyin Channels import qilamiz
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import consultations.routing  # WebSocket routing
+import consultations.routing
 
 application = ProtocolTypeRouter(
     {
--- config/settings.py
+++ config/settings.py
@@ -108,24 +108,24 @@ CHANNEL_LAYERS = {
 # }
 # This configuration in the Django settings file is defining the default database connection settings
 # for PostgreSQL. Here's what each key represents:
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "medmapp",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
-
-
 # DATABASES = {
-#     "default": dj_database_url.parse(
-#         "postgresql://postgres:oCvZGMRaaTIjYZfTXfAjSNbZvZHKoniR@gondola.proxy.rlwy.net:42842/railway",
-#         conn_max_age=600,
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "medmapp",
+#         "USER": "postgres",
+#         "PASSWORD": "1234",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
 # }
+
+
+DATABASES = {
+    "default": dj_database_url.parse(
+        "postgresql://postgres:oCvZGMRaaTIjYZfTXfAjSNbZvZHKoniR@gondola.proxy.rlwy.net:42842/railway",
+        conn_max_age=600,
+    )
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
