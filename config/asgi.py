4c62d393a195286173c33b3a36324ec783fb7ddb
ruxshona2103 <ruxshonaaglamkhujayeva@gmail.com>
2025-10-08 18:10:51 +0500

    updated

--- config/asgi.py
+++ config/asgi.py
@@ -1,19 +1,21 @@
 import os
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import consultations.routing
+# Agar websockets routing faylingiz bo‘lsa (masalan, consultations.routing.application yoki patients.routing.websocket_urlpatterns)
+try:
+    from consultations.routing import websocket_urlpatterns as chat_ws
+except Exception:
+    chat_ws = []
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(consultations.routing.websocket_urlpatterns)
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(chat_ws)
+    ),
+})
