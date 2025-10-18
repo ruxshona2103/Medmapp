41869a73873098b69be5fd175ad4ab36ef35e177
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2025-10-18 13:39:55 +0500

    integrating with operator site

--- applications/urls_operator.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views_operator import OperatorApplicationsView, ChangeApplicationStatusView
-
-urlpatterns = [
-    path('operator/applications/', OperatorApplicationsView.as_view(), name='operator_applications'),
-    path('operator/applications/<int:pk>/change-status/', ChangeApplicationStatusView.as_view(), name='change_status'),
-]
