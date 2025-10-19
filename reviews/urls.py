9f1c5165c45affdec8a5bd1745dc9b5f95300004
ruxshona2103 <ruxshonaaglamkhujayeva@gmail.com>
2025-10-19 17:09:48 +0500

    ordering added to stage and patient

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClinicViewSet, DoctorViewSet, ReviewViewSet
-
 router = DefaultRouter()
-router.register(r"clinics", ClinicViewSet, basename="clinic")
-router.register(r"doctors", DoctorViewSet, basename="doctor")
-router.register(r"reviews", ReviewViewSet, basename="review")
+router.register(r"clinics", ClinicViewSet, basename="clinics")
+router.register(r"doctors", DoctorViewSet, basename="doctors")
+router.register(r"reviews", ReviewViewSet, basename="reviews")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+]
