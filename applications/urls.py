4070af1be22e8a5bbfaf3c803856d51bcc52f2bf
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2025-10-23 05:39:57 +0500

    integrating backend with frontend , solving problems

--- applications/urls.py
+++ applications/urls.py
@@ -2,20 +2,35 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     ApplicationViewSet,
+    PatientApplicationViewSet,
+    CompletedApplicationViewSet,
     DocumentListCreateView,
     ChangeApplicationStageView,
     ChangeApplicationStatusView,
-    CompletedApplicationViewSet,
 )
 
 router = DefaultRouter()
-router.register(r"applications", ApplicationViewSet, basename="applications")
-router.register(r"completed-applications", CompletedApplicationViewSet, basename="completed-applications")
+router.register(r'applications', ApplicationViewSet, basename='application')
+router.register(r'completed-applications', CompletedApplicationViewSet, basename='completed-application')
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path('', include(router.urls)),
 
-    path("applications/<int:application_id>/documents/", DocumentListCreateView.as_view(), name="application-documents"),
-    path("applications/<int:application_id>/change-stage/", ChangeApplicationStageView.as_view(), name="application-change-stage"),
-    path("applications/<int:application_id>/change-status/", ChangeApplicationStatusView.as_view(), name="application-change-status"),
+    # 👤 Patient ID bo'yicha arizalar
+    path('applications/patient/<int:patient_id>/',
+         PatientApplicationViewSet.as_view({'get': 'list'}),
+         name='patient-applications-list'),
+
+    path('applications/patient/<int:patient_id>/<int:pk>/',
+         PatientApplicationViewSet.as_view({'get': 'retrieve'}),
+         name='patient-applications-detail'),
+    path('applications/<int:application_id>/documents/',
+         DocumentListCreateView.as_view(),
+         name='application-documents'),
+    path('applications/<int:application_id>/change-stage/',
+         ChangeApplicationStageView.as_view(),
+         name='change-application-stage'),
+    path('applications/<int:application_id>/change-status/',
+         ChangeApplicationStatusView.as_view(),
+         name='change-application-status'),
 ]
