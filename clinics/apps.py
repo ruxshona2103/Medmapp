fd6382970b21cf5e56a6db9ec52fa55fbb8c7a75
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2025-11-04 14:21:06 +0500

    clinics and doctors are added

--- clinics/apps.py
+++ clinics/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class ReviewsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "reviews"
+    name = "clinics"
     verbose_name = "Reviews & Clinics"
