7255a623ebc5b5fa665e67583a42a801c3b6dee5
ruxshona2103 <ruxshonaaglamkhujayeva@gmail.com>
2025-11-08 15:37:30 +0500

    conversations updated files url fixed

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import StageViewSet, TagViewSet
-from .health_check import health_check, test_images
 
 router = DefaultRouter()
 router.register(r"stages", StageViewSet, basename="stages")
 router.register(r"tags", TagViewSet, basename="tags")
 
-urlpatterns = [
-    path("health/", health_check, name="health-check"),
-    path("test-images/", test_images, name="test-images"),
-] + router.urls
