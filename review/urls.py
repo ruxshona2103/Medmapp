859a6fbec204fc6b3c99abdc0c7d8aeef39b41f0
ruxshona2103 <ruxshonaaglamkhujayeva@gmail.com>
2025-11-08 15:29:06 +0500

    conversations updated files url fixed

--- review/urls.py
+++ review/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from review.views import ReviewViewSet, BlogPostViewSet
 
 router = DefaultRouter()
-router.register("review", ReviewViewSet)
-router.register("blog", BlogPostViewSet)
+router.register("review", ReviewViewSet, basename="review")
+router.register("blog", BlogPostViewSet, basename="blog")
 urlpatterns = router.urls
