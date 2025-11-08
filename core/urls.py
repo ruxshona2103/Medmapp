1ed1696bd1415c91c9b2457a8598745a3e77a960
ruxshona2103 <ruxshonaaglamkhujayeva@gmail.com>
2025-11-08 15:42:09 +0500

    problems fixed

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import StageViewSet, TagViewSet
 
 router = DefaultRouter()
 router.register(r"stages", StageViewSet, basename="stages")
 router.register(r"tags", TagViewSet, basename="tags")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
 
