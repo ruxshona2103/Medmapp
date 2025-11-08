15adddd9e590540c5dfacbc095a80f01564a9798
ruxshona2103 <ruxshonaaglamkhujayeva@gmail.com>
2025-11-08 15:29:02 +0500

    conversations updated files url fixed

--- consultations/urls.py
+++ consultations/urls.py
@@ -4,20 +4,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+
 router = DefaultRouter()
 router.register(r"conversations", views.ConversationViewSet, basename="conversation")
 router.register(r"messages", views.MessageViewSet, basename="message")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path(
-        "conversations/operator/<int:patient_id>/messages/",
-        views.ConversationViewSet.as_view(
-            {
-                "get": "operator_conversation_messages",
-                "post": "operator_conversation_messages",
-            }
-        ),
-        name="operator-conversation-messages",
-    ),
 ]
