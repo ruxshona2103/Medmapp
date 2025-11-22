1f3af72d731a5406850f752a71c7cf46748a94d6
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2025-11-22 05:37:15 +0500

    world-clinics added

--- clinics/urls.py
+++ clinics/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     CountryViewSet, CityViewSet, SpecialtyViewSet,
-    ClinicViewSet,
+    ClinicViewSet, WorldClinicViewSet,
 )
 
 router = DefaultRouter()
@@ -11,4 +11,5 @@ router.register(r"cities", CityViewSet, basename="cities")
 router.register(r"specialties", SpecialtyViewSet, basename="specialties")
 
 router.register(r"clinics", ClinicViewSet, basename="clinics")
+router.register(r"world-clinics", WorldClinicViewSet, basename="world-clinics")
 urlpatterns = [path("", include(router.urls))]
