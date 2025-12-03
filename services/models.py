c8525db4688b769e1a443edee5f78080ec231493
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2025-12-03 06:52:07 +0500

    tags added to orders

--- services/models.py
+++ services/models.py
@@ -17,6 +17,7 @@ class Hotel(models.Model):
     image = models.ImageField(upload_to="hotels/", null=True, blank=True)
     stars = models.PositiveSmallIntegerField(default=3)
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    tags = models.ManyToManyField('core.Tag', blank=True, related_name='hotels')
 
     def __str__(self):
         return self.name
@@ -29,6 +30,7 @@ class Booking(models.Model):
     end_date = models.DateField()
     guests = models.PositiveIntegerField()
     created_at = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField('core.Tag', blank=True, related_name='bookings')
 
     def __str__(self):
         return f"{self.user} → {self.hotel.name} ({self.start_date} - {self.end_date})"
@@ -38,6 +40,7 @@ class VisaRequest(TimeStampedModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="viza_sorovlari")
     passport_scan = models.FileField( upload_to="visa/passports/", validators=[validate_file_type])
     note = models.CharField(max_length=500, blank=True)
+    tags = models.ManyToManyField('core.Tag', blank=True, related_name='visa_requests')
 
 
 class TransferRequest(TimeStampedModel):
@@ -45,15 +48,18 @@ class TransferRequest(TimeStampedModel):
     flight_number = models.CharField(max_length=50)
     arrival_datetime = models.DateTimeField(validators=[validate_future_dt])
     ticket_scan = models.FileField(upload_to="transfer/tickets/",blank=True,null=True,validators=[validate_file_type])
+    tags = models.ManyToManyField('core.Tag', blank=True, related_name='transfer_requests')
 
 
 class TranslatorRequest(TimeStampedModel):
     user = models.ForeignKey( User,on_delete=models.CASCADE,related_name="tarjimon_sorovlari")
     language = models.CharField(max_length=50)
     requirements = models.TextField(blank=True)
+    tags = models.ManyToManyField('core.Tag', blank=True, related_name='translator_requests')
 
 
 class SimCardRequest(TimeStampedModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="simkarta_sorovlari")
     passport_scan = models.FileField(upload_to="simcard/passports/",validators=[validate_file_type])
     note = models.CharField(max_length=500, blank=True)
+    tags = models.ManyToManyField('core.Tag', blank=True, related_name='simcard_requests')
